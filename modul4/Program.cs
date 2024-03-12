3203fcccc915fe7b2f432f0388e63550241bf073
GinaSoraya <ginasry08@gmail.com>
2024-03-13 01:52:07 +0700

    menambahkan implementasi dengan table-deiven

--- modul4/Program.cs
+++ modul4/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics;
+
+public class Program
+{
+    public enum KodePos
+    {
+        Batununggal,
+        Kunjangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Margasari,
+        Sekejati,
+        Kebonwaru,
+        Maleer,
+        Samoja
+    }
+
+    public static int getKodePos(KodePos pos)
+    {
+        int[] outputKode = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return outputKode[(int)pos];
+    }
+
+    public static void Main(string[] args)
+    {
+        KodePos Pos = KodePos.Jatisari;
+        int kodepos = getKodePos(Pos);
+        Console.WriteLine("Kode Pos pada kelurahan " + Pos + " aladah " + kodepos);
+    }
+}
